fb9d8ffa050510d49fd7d77cecdbc0ad48f54411
UntamedLotus <ruttikajadhav@gmail.com>
2022-08-26 12:14:01 +0530

    Initial commits

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import "./hero.scss";
+// import { HashLink as Link } from "react-router-hash-link";
 
 function Hero() {
   return (
     <div className="hero">
+      {/* <div className="nav">
+        <div className="nav-logo">
+          <h2> Ruttika</h2>
+        </div>
+        <div className="nav-links"></div>
+      </div> */}
       <div className="about">
         <h2>Jadhav Ruttika</h2>
         <p className="role">Front-end Developer</p>
@@ -15,21 +22,14 @@ function Hero() {
         </p>
       </div>
       <div className="about-img">
-        <img
+        {/* <img
           className="img"
           src="https://64.media.tumblr.com/c41035ddb2eb612e53649324fdc1a75d/413a65c8cfce3d3f-b0/s540x810/e0335ae627eb1b9ebac0a2b913cf10e5649cf7af.gifv"
-        />
+          alt="gif by mienar on tumbler"
+        /> */}
       </div>
     </div>
   );
 }
 
 export default Hero;
-
-{
-  /* 
-  
-     <img className="img-A" src="https://i.postimg.cc/Jz1msRPH/coding.gif" />
-
-  <img src="https://64.media.tumblr.com/fe6d6866c5f3902586116f472a2ab20f/921683666be3fa68-8a/s540x810/90260b81c89a1cc7d3f0bdabf9096d7530e3f83d.gifv" /> */
-}
---
+++ src/components/Raw.md
@@ -0,0 +1,5 @@
+<!--
+
+     <img className="img-A" src="https://i.postimg.cc/Jz1msRPH/coding.gif" />
+
+<img src="https://64.media.tumblr.com/fe6d6866c5f3902586116f472a2ab20f/921683666be3fa68-8a/s540x810/90260b81c89a1cc7d3f0bdabf9096d7530e3f83d.gifv" />  -->
